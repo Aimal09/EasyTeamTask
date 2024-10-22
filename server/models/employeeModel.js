393bfd27e5f9e86f10bb8132f9e9233135e012eb
Aimal <aimalaemi@gmail.com>
2024-10-22 20:20:37 +0500

    email added in employee model

--- server/models/employeeModel.js
+++ server/models/employeeModel.js
@@ -10,6 +10,10 @@ const employeeSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    email: {
+        type: String,
+        required: true,
+    },
     password: {
         type: String,
         required: true,
