bc3d466d45656b8eb627bbf025e1fcdca0849823
Aimal <aimalaemi@gmail.com>
2024-10-24 00:08:30 +0500

    Add Layout with EasyTeamProvider

---
+++ components/Layout.jsx
@@ -0,0 +1,19 @@
+import React, { } from "react";
+import { EasyTeamProvider } from "@easyteam/ui";
+import { BASE_PATH } from "../config/api";
+import { useAppState } from "../context/AppStateContext";
+
+const Layout = ({ children }) => {
+  const { state } = useAppState();
+
+  return (
+    <EasyTeamProvider
+      token={state.token}
+      employees={state.employees}
+      basePath={BASE_PATH}>
+        {children}
+    </EasyTeamProvider>
+  );
+};
+
+export default Layout;
