93d75ce1ae4e95f8e3bf80951ea5b7c8a19a6b85
Aimal <aimalaemi@gmail.com>
2024-10-24 00:07:19 +0500

    Code cleanup

--- App.tsx
+++ App.tsx
@@ -1,25 +1,12 @@
-import { Clock, EasyTeamProvider } from "@easyteam/ui";
 import React, { useState } from 'react';
-import { Button, Text, View } from "react-native";
-import Employee from "./server/models/employeeModel";
-import Login from "./pages/login/Login";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { AuthProvider } from "./pages/login/AuthProvider";
-import Home from "./pages/home/Home";
-
-const Stack = createNativeStackNavigator();
+import { AppStateProvider } from "./context/AppStateContext.js";
+import MainNavigator from "./navigation/MainNavigator";
 
 function App(): React.JSX.Element {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthProvider>
+    <AppStateProvider>
+      <MainNavigator />
+    </AppStateProvider>
   );
 }
 
