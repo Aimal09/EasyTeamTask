2166602df1a1de1fe272afe01ccc1412d2a91b6c
Aimal <aimalaemi@gmail.com>
2024-10-24 00:10:16 +0500

    Add Timesheet Employees screen

---
+++ screens/TimesheetEmployeeScreen.jsx
@@ -0,0 +1,21 @@
+import React, { useRef, useState } from "react";
+import { Timesheet } from "@easyteam/ui";
+
+const TimesheetScreen = () => {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const ref = useRef(null);
+  return (
+    <Timesheet
+      ref={ref}
+      onDateRangeChange={(newStartDate, newEndDate) => {
+        setStartDate(newStartDate);
+        setEndDate(newEndDate);
+      }}
+      startDate={startDate}
+      endDate={endDate}
+    />
+  );
+};
+
+export default TimesheetScreen;
