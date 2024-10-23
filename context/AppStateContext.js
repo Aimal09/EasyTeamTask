b19f81e9eb4fba16846027ceab7c2beb316e3d71
Aimal <aimalaemi@gmail.com>
2024-10-24 00:08:53 +0500

    Add State context

---
+++ context/AppStateContext.js
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+const AppStateContext = createContext();
+
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return { ...state, user: action.payload };
+    case 'SET_TOKEN':
+      return { ...state, token: action.payload };
+    case 'SET_EMPLOYEES':
+      return { ...state, employees: action.payload };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  user: null,
+  token:'',
+  employees:[]
+};
+
+export const AppStateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
+  return (
+    <AppStateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+export const useAppState = () => useContext(AppStateContext);
